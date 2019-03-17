5da3672658a6152ab8a3c9f9b5acfb0be147c321
szabobarna96 <szabo.barna96@gmail.com>
2019-03-17 20:57:38 +0100

    Kalandozás kiszervezése külön osztályba

--- src/Game/Enemy/Enemy.java
+++ src/Game/Enemy/Enemy.java
@@ -1,14 +1,15 @@
 package Game.Enemy;
 
 import java.io.*;
+import java.util.Random;
 
 public class Enemy {
-    public String Name;
-    public int Health;
-    public int Level;
-    public int Damage;
-    public int XpGiven;
-    public int GoldGiven;
+    private String Name;
+    private int Health;
+    private int Level;
+    private int Damage;
+    private int XpGiven;
+    private int GoldGiven;
     public String Type;
 
     public Enemy(){
@@ -16,6 +17,16 @@ public class Enemy {
     }
 
     private void CreateEnemy(){
+        Random rand = new Random();
+        Name = "Goblin";
+        Level = 1;
+        Health = rand.nextInt(10);
+        Damage = rand.nextInt(3)+1;
+        XpGiven = Damage;
+        GoldGiven = Damage*2;
+    }
 
+    public String getName(){
+        return Name;
     }
 }
