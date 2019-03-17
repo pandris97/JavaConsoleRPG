24c33234a0e83c6b84b3c1ce7373fb4bd40964d4
szabobarna96 <szabo.barna96@gmail.com>
2019-03-17 20:57:38 +0100

    Üres Enemy osztály létrehozása

---
+++ src/Game/Enemy/Enemy.java
@@ -0,0 +1,21 @@
+package Game.Enemy;
+
+import java.io.*;
+
+public class Enemy {
+    public String Name;
+    public int Health;
+    public int Level;
+    public int Damage;
+    public int XpGiven;
+    public int GoldGiven;
+    public String Type;
+
+    public Enemy(){
+        CreateEnemy();
+    }
+
+    private void CreateEnemy(){
+
+    }
+}
