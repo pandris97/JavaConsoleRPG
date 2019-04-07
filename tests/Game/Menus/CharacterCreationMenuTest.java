70d8005db076763a0764c815757e5b4d93fe1d59
pandris97 <andras.palinkas97@gmail.com>
2019-04-07 23:41:37 +0200

    test creation

---
+++ tests/Game/Menus/CharacterCreationMenuTest.java
@@ -0,0 +1,17 @@
+package Game.Menus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterCreationMenuTest {
+
+    @Test
+    void runMenu() {
+    }
+
+    @Test
+    void printMenu() {
+
+    }
+}
