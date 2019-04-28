3e9ac807bb25f240e15f062340311a1697a575d7
pandris97 <andras.palinkas97@gmail.com>
2019-04-28 21:23:42 +0200

    fix attempt on travis.yml and Doxyfile

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 import Game.Menus.MainMenu;
 
+/*! This is the main class */
+
 public class Main {
+        /** the main function, this creates a main menu object and runs the program 
+     *  
+     */
     public static void main(String[] args) {
         MainMenu mMenu = new MainMenu();
         mMenu.runMenu();
